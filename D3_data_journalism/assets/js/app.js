34e15dbe70946729f80b966c798336b9539b9c64
NikitaCase <69657536+NikitaCase@users.noreply.github.com>
2021-01-28 13:42:48 -0500

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,28 @@
-// @TODO: YOUR CODE HERE!
+var svg_width = 900;
+var svg_height = 500;
+
+//  ;;;;;;;;;;;;;;;;;;;;;;;;;
+
+var margin = {
+  top:40,
+  right: 40,
+  bottom: 60,
+  left: 60
+};
+
+
+var width = svg_width - margin.left - margin.right;
+var height = svg_height - margin.top - margin.bottom;
+
+
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svg_width)
+  .attr("height", svg_height)
+
+
+var chart = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+
