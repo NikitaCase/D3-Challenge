038a66118022076f876c9f80f2371b0a86d27b6b
KitaTanya <69657536+KitaTanya@users.noreply.github.com>
2021-01-28 18:05:52 -0500

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -4,10 +4,10 @@ var svg_height = 500;
 //  ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 var margin = {
-  top:40,
-  right: 40,
-  bottom: 60,
-  left: 60
+    top: 40,
+    right: 40,
+    bottom: 60,
+    left: 60
 };
 
 
@@ -16,13 +16,41 @@ var height = svg_height - margin.top - margin.bottom;
 
 
 var svg = d3
-  .select("#scatter")
-  .append("svg")
-  .attr("width", svg_width)
-  .attr("height", svg_height)
+    .select("#scatter")
+    .append("svg")
+    .attr("width", svg_width)
+    .attr("height", svg_height)
 
 
 var chart = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
 
+
+d3.csv('D3_data_journalism/assets/data/data.csv').then(function(data) {
+    data.forEach(function(row) {
+        row.healthcare = +row.healthcare
+        row.poverty = +row.poverty
+    })
+
+    var x_scale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.poverty)])
+        .range([0, width]);
+
+    var y_scale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.healthcare)])
+        .range([height, 0]);
+
+    var x_axis = d3.axisBottom(x_scale)
+    var y_axis = d3.axisLeft(y_scale);
+
+    chart.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(x_axis);
+
+    chart.append("g")
+        .call(y_axis);
+
+
+
+}).catch(function(error) { console.log(error) })
